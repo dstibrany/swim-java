a8eb1d4a4e0296482fe23a050f49f9b76f220264
Dave Stibrany <dstibrany@gmail.com>
2019-11-13 20:41:07 -0500

    Use constants to increase readability in simulation tests

--- src/integrationTest/java/ca/davestibrany/swimjava/SimulationTests.java
+++ src/integrationTest/java/ca/davestibrany/swimjava/SimulationTests.java
@@ -15,19 +15,22 @@ class SimulationTests {
 
     @Test
     void checkMemberListAfterJoins() throws InterruptedException, ExecutionException {
-        // TODO: name constants
+        int maxRounds = 6;
+        int node1Port = 5555;
+        int node2Port = 5556;
+        int node3Port = 5557;
         SimulationQueues simulationQueues = new SimulationQueues();
         SimulationNode node1 = new SimulationNode.Builder()
                 .withJoinTime(1)
-                .build(5555, simulationQueues);
+                .build(node1Port, simulationQueues);
         SimulationNode node2 = new SimulationNode.Builder()
                 .withJoinTime(2)
-                .build(5556, simulationQueues);
+                .build(node2Port, simulationQueues);
         SimulationNode node3 = new SimulationNode.Builder()
                 .withJoinTime(3)
-                .build(5557, simulationQueues);
+                .build(node3Port, simulationQueues);
         List<SimulationNode> nodes = Arrays.asList(node1, node2, node3);
-        Simulation simulation = new Simulation(nodes, 6);
+        Simulation simulation = new Simulation(nodes, maxRounds);
 
         simulation.run();
 
@@ -38,19 +41,23 @@ class SimulationTests {
 
     @Test
     void testDetectionOfSingleFailure() throws InterruptedException, ExecutionException {
+        int maxRounds = 20;
+        int node1Port = 5555;
+        int node2Port = 5556;
+        int node3Port = 5557;
         SimulationQueues simulationQueues = new SimulationQueues();
         SimulationNode node1 = new SimulationNode.Builder()
                 .withJoinTime(1)
-                .build(5555, simulationQueues);
+                .build(node1Port, simulationQueues);
         SimulationNode node2 = new SimulationNode.Builder()
                 .withJoinTime(2)
-                .build(5556, simulationQueues);
+                .build(node2Port, simulationQueues);
         SimulationNode node3 = new SimulationNode.Builder()
                 .withJoinTime(3)
                 .withFailureTime(5)
-                .build(5557, simulationQueues);
+                .build(node3Port, simulationQueues);
         List<SimulationNode> nodes = Arrays.asList(node1, node2, node3);
-        Simulation simulation = new Simulation(nodes, 20);
+        Simulation simulation = new Simulation(nodes, maxRounds);
 
         simulation.run();
 
@@ -63,20 +70,24 @@ class SimulationTests {
 
     @Test
     void testDetectionOfMultipleFailures() throws InterruptedException, ExecutionException {
+        int maxRounds = 20;
+        int node1Port = 5555;
+        int node2Port = 5556;
+        int node3Port = 5557;
         SimulationQueues simulationQueues = new SimulationQueues();
         SimulationNode node1 = new SimulationNode.Builder()
                 .withJoinTime(1)
-                .build(5555, simulationQueues);
+                .build(node1Port, simulationQueues);
         SimulationNode node2 = new SimulationNode.Builder()
                 .withJoinTime(2)
                 .withFailureTime(7)
-                .build(5556, simulationQueues);
+                .build(node2Port, simulationQueues);
         SimulationNode node3 = new SimulationNode.Builder()
                 .withJoinTime(3)
                 .withFailureTime(5)
-                .build(5557, simulationQueues);
+                .build(node3Port, simulationQueues);
         List<SimulationNode> nodes = Arrays.asList(node1, node2, node3);
-        Simulation simulation = new Simulation(nodes, 20);
+        Simulation simulation = new Simulation(nodes, maxRounds);
 
         simulation.run();
 
@@ -90,21 +101,25 @@ class SimulationTests {
     @Test
     void testWithPacketLossAndNoFailures() throws ExecutionException, InterruptedException {
         double dropProbability = 0.05;
+        int maxRounds = 30;
+        int node1Port = 5555;
+        int node2Port = 5556;
+        int node3Port = 5557;
         SimulationQueues simulationQueues = new SimulationQueues();
         SimulationNode node1 = new SimulationNode.Builder()
                 .withJoinTime(1)
                 .withDropProbability(dropProbability)
-                .build(5555, simulationQueues);
+                .build(node1Port, simulationQueues);
         SimulationNode node2 = new SimulationNode.Builder()
                 .withJoinTime(3)
                 .withDropProbability(dropProbability)
-                .build(5556, simulationQueues);
+                .build(node2Port, simulationQueues);
         SimulationNode node3 = new SimulationNode.Builder()
                 .withJoinTime(8)
                 .withDropProbability(dropProbability)
-                .build(5557, simulationQueues);
+                .build(node3Port, simulationQueues);
         List<SimulationNode> nodes = Arrays.asList(node1, node2, node3);
-        Simulation simulation = new Simulation(nodes, 30);
+        Simulation simulation = new Simulation(nodes, maxRounds);
 
         simulation.run();
 
@@ -116,22 +131,26 @@ class SimulationTests {
     @Test
     void testWithPacketLossAndSingleFailure() throws ExecutionException, InterruptedException {
         double dropProbability = 0.1;
+        int maxRounds = 30;
+        int node1Port = 5555;
+        int node2Port = 5556;
+        int node3Port = 5557;
         SimulationQueues simulationQueues = new SimulationQueues();
         SimulationNode node1 = new SimulationNode.Builder()
                 .withJoinTime(1)
                 .withDropProbability(dropProbability)
                 .withFailureTime(13)
-                .build(5555, simulationQueues);
+                .build(node1Port, simulationQueues);
         SimulationNode node2 = new SimulationNode.Builder()
                 .withJoinTime(3)
                 .withDropProbability(dropProbability)
-                .build(5556, simulationQueues);
+                .build(node2Port, simulationQueues);
         SimulationNode node3 = new SimulationNode.Builder()
                 .withJoinTime(8)
                 .withDropProbability(dropProbability)
-                .build(5557, simulationQueues);
+                .build(node3Port, simulationQueues);
         List<SimulationNode> nodes = Arrays.asList(node1, node2, node3);
-        Simulation simulation = new Simulation(nodes, 30);
+        Simulation simulation = new Simulation(nodes, maxRounds);
 
         simulation.run();
 
@@ -145,34 +164,40 @@ class SimulationTests {
     @Test
     void test5NodesWithPacketLossAndNoFailures() throws ExecutionException, InterruptedException {
         double dropProbability = 0.05;
+        int maxRounds = 30;
+        int node1Port = 5555;
+        int node2Port = 5556;
+        int node3Port = 5557;
+        int node4Port = 5558;
+        int node5Port = 5559;
         SimulationQueues simulationQueues = new SimulationQueues();
         SimulationNode node1 = new SimulationNode.Builder()
                 .withJoinTime(1)
                 .withDropProbability(dropProbability)
                 .withSuspicionTimeout(1000)
-                .build(5555, simulationQueues);
+                .build(node1Port, simulationQueues);
         SimulationNode node2 = new SimulationNode.Builder()
                 .withJoinTime(3)
                 .withDropProbability(dropProbability)
                 .withSuspicionTimeout(1000)
-                .build(5556, simulationQueues);
+                .build(node2Port, simulationQueues);
         SimulationNode node3 = new SimulationNode.Builder()
                 .withJoinTime(5)
                 .withDropProbability(dropProbability)
                 .withSuspicionTimeout(1000)
-                .build(5557, simulationQueues);
+                .build(node3Port, simulationQueues);
         SimulationNode node4 = new SimulationNode.Builder()
                 .withJoinTime(7)
                 .withDropProbability(dropProbability)
                 .withSuspicionTimeout(1000)
-                .build(5558, simulationQueues);
+                .build(node4Port, simulationQueues);
         SimulationNode node5 = new SimulationNode.Builder()
                 .withJoinTime(9)
                 .withDropProbability(dropProbability)
                 .withSuspicionTimeout(1000)
-                .build(5559, simulationQueues);
+                .build(node5Port, simulationQueues);
         List<SimulationNode> nodes = Arrays.asList(node1, node2, node3, node4, node5);
-        Simulation simulation = new Simulation(nodes, 30);
+        Simulation simulation = new Simulation(nodes, maxRounds);
 
         simulation.run();
 
